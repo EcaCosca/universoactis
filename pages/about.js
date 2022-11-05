e5b4219006f0c5760db70f9d28300c9c45af7d21
EcaCosca <enriquecoscarelli@gmail.com>
2022-11-05 22:06:10 +0100

    Meta data

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,18 @@
+import Head from 'next/head'
+
 const About = () => {
   return (
-    <div>
-        <h1>About</h1>
-        <p>lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum</p>
-        <p>lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum</p>
-    </div>
+    <>
+      <Head>
+        <title>Universo Actis | About</title>
+        <meta name="keywords" content="actis"/>
+      </Head>
+      <div>
+          <h1>About</h1>
+          <p>lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum</p>
+          <p>lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum</p>
+      </div>
+    </>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,18 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>lorem ipsumloremlorem ipsumloremlorem ipsumloremlorem ipsumloremlorem ipsumloremlorem ipsumloremlorem ipsumlorem</p>
-      <p className={styles.text}>lorem ipsumloremlorem ipsumloremlorem ipsumloremlorem ipsumloremlorem ipsumloremlorem ipsumloremlorem ipsumlorem</p>
-      <Link className={styles.btn} href="/ninjas/ninja">Ninja Listing</Link>
-    </div>
+    <>
+      <Head>
+        <title>Universo Actis | Home</title>
+        <meta name="keywords" content="actis"/>
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>lorem ipsumloremlorem ipsumloremlorem ipsumloremlorem ipsumloremlorem ipsumloremlorem ipsumloremlorem ipsumlorem</p>
+        <p className={styles.text}>lorem ipsumloremlorem ipsumloremlorem ipsumloremlorem ipsumloremlorem ipsumloremlorem ipsumloremlorem ipsumlorem</p>
+        <Link className={styles.btn} href="/ninjas/ninja">Ninja Listing</Link>
+      </div>
+    </>
     )
 }
 
