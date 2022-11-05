c132176b9bedfff5565c9a0528eab26c10fdcd39
EcaCosca <enriquecoscarelli@gmail.com>
2022-11-05 15:55:14 +0100

    Done

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Footer from '../components/footer'
-import Navbar from '../components/navbar'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
