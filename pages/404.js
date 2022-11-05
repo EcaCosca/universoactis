99a34e96784613fc51ac37e223824df4cb774f5a
EcaCosca <enriquecoscarelli@gmail.com>
2022-11-05 21:47:09 +0100

    Adding link

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,11 @@
+import Link from 'next/link'
+
 const NotFound = () => {
   return (
     <div className="not-found">
         <h1>Ooooops...</h1>
         <h2>That page cannot be found.</h2>
-        <p>Go back to the </p>
+        <p>Go back to the <Link href="/"></Link></p>
     </div>
   )
 }
