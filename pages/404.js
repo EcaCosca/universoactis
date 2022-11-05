3036ad4f636c0d9cfc9a53789b99a0c39424df99
EcaCosca <enriquecoscarelli@gmail.com>
2022-11-05 21:46:17 +0100

    Adding text

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,9 @@
 const NotFound = () => {
   return (
     <div className="not-found">
-        
+        <h1>Ooooops...</h1>
+        <h2>That page cannot be found.</h2>
+        <p>Go back to the </p>
     </div>
   )
 }
